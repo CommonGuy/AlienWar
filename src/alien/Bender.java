4ca3bb29de3b91a0b08afa49e2b6f40ad8e7c631
Michael Lyons <michael@fireside21.com>
2014-07-12 11:53:21 -0400

    Bender: Lots of bugfixes:
    - loop break -> continue
    - image processing transposed the axes
    - tweak desirability matrices

--- src/alien/Bender.java
+++ src/alien/Bender.java
@@ -12,13 +12,14 @@ import java.util.HashMap;
 /// Whales and Eagles are also bendable. =)
 public class Bender extends Alien {
 
+    private static final boolean DEBUG = false;
     private final int LIF = 0, STR = 1, DEF = 2, VIS = 3, CLV = 4;
     protected static HashMap<Character, Integer> preyDesirability = new HashMap<Character, Integer>() {{
-        put('A', -9);
+        put('A', -5);
         put('W',  5);
         put('C',  0); // Originally -2, but Cows don't attack
-        put('T',  1);
-        put('E',  2);
+        put('T',  2);
+        put('E',  3);
         put('H',  4);
     }};
     private static final int bananaTweak = -2;
@@ -30,10 +31,10 @@ public class Bender extends Alien {
     }};
 
     public void setAbilityPoints(float[] abilities) {
-        abilities[LIF] = 2.5f; // Shiny metal ass
-        abilities[STR] = 7.5f; // Bending strength
+        abilities[LIF] = 3.5f; // Shiny metal ass
+        abilities[STR] = 5.5f; // Bending strength
         abilities[DEF] = 0;
-        abilities[VIS] = 0;    // Binocular eyes
+        abilities[VIS] = 1;    // Binocular eyes
         abilities[CLV] = 0;
     }
 
@@ -54,14 +55,18 @@ public class Bender extends Alien {
         int vision = getVisionFieldsCount();
         // I am at fields[vision][vision]
 
+        if (DEBUG) {
+            System.out.format("\n----- %s -----\n", this);
+        }
+
         float[][] scoringMap = new float[fields.length][fields.length];
-        for (int x = 0; x < fields.length; x++) {
-            for (int y = 0; y < fields.length; y++) {
+        for (int y = 0; y < fields.length; y++) {
+            for (int x = 0; x < fields.length; x++) {
 
                 // Ignore my square and blanks
                 if (x == vision && y == vision ||
                     fields[x][y] == ' ') {
-                    break;
+                    continue;
                 }
 
                 // Check out the prey 8^]
@@ -71,14 +76,44 @@ public class Bender extends Alien {
                 // If we know where it's going, score tiles accordingly...
                 if (preyMovement.containsKey(organism)) {
                     Move preyMove = preyMovement.get(organism);
-                    scoringMap[x + preyMove.getXOffset()][y + preyMove.getYOffset()] += desirability;
+                    if (DEBUG) {
+                        System.out.println(String.format("Prey %s will move %s", organism, preyMove));
+                    }
+                    int newPreyX = x + preyMove.getXOffset();
+                    int newPreyY = y + preyMove.getYOffset();
+                    try {
+                        scoringMap[newPreyX][newPreyY] += desirability;
+                        if (DEBUG) {
+                            System.out.println(String.format(
+                                "Adding %.1f to %d, %d",
+                                desirability,
+                                newPreyX,
+                                newPreyY));
+                        }
+                    }
+                    catch(Exception e) {
+                        if (DEBUG) {
+                            System.out.println(String.format(
+                                "Failed adding %.1f to %d, %d",
+                                desirability,
+                                newPreyX,
+                                newPreyY));
+                        }
+                    }
                 }
                 // ...otherwise, divide its score between its
                 //    available moves...
                 else {
-                    for (int i = x - 1; i < x + 1; i++) {
-                        for (int j = y - 1; j < y + 1; j++) {
-                            scoringMap[i][j] += desirability / 9.;
+                    for (int j = y - 1; j <= y + 1; j++) {
+                        for (int i = x - 1; i <= x + 1; i++) {
+                            try {
+                                scoringMap[i][j] += desirability / 9.;
+                            }
+                            catch (Exception e) {
+                                if (DEBUG) {
+                                    //System.out.println(e);
+                                }
+                            }
                         }
                     }
                 }
@@ -101,9 +136,12 @@ public class Bender extends Alien {
         // +-----------+
         float bestScore = -10;
         int[] bestXY = new int[2];
-        for (int x = vision - 1; x < vision + 1; x++) {
-            for (int y = vision - 1; y < vision + 1; y++) {
+        for (int y = vision - 1; y <= vision + 1; y++) {
+            for (int x = vision - 1; x <= vision + 1; x++) {
 
+                if (DEBUG) {
+                    System.out.format("\nx:%d, y:%d", x, y);
+                }
                 // Look for the best score, but if scores
                 // are tied, try for most southwest high score
                 if (scoringMap[x][y] > bestScore ||
@@ -114,15 +152,40 @@ public class Bender extends Alien {
                     bestScore = scoringMap[x][y];
                     bestXY[0] = x;
                     bestXY[1] = y;
+                    if (DEBUG) {
+                        System.out.format("\nBest score of %.1f found at %d, %d", bestScore, x, y);
+                    }
                 }
             }
         }
 
+        if (DEBUG) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("\n-----\n");
+            for (int y = 0; y < fields.length; y++) {
+                for (int x = 0; x < fields.length; x++) {
+                    sb.append(String.format("%5s", fields[x][y]));
+                }
+                sb.append("\n");
+            }
+            for (int y = 0; y < scoringMap.length; y++) {
+                for (int x = 0; x < scoringMap.length; x++) {
+                    sb.append(String.format("%5.1f", scoringMap[x][y]));
+                }
+                sb.append("\n");
+            }
+            System.out.println(sb.toString());
+        }
+
         // If something looks tasty, go for it :^F
-        if (bestScore > 1) {
+        if (bestScore > 0.5) {
             for (Move m : Move.values()) {
                 if (m.getXOffset() == bestXY[0] - vision &&
                     m.getYOffset() == bestXY[1] - vision) {
+                    if (DEBUG) {
+                        System.out.println("Using immediate circumstances.");
+                        System.out.println(m);
+                    }
                     return m;
                 }
             }
@@ -136,8 +199,8 @@ public class Bender extends Alien {
         // vision data with updated score rankings, but
         // that would be hard. :P
         float westScore = 0, southScore = 0, southWestScore = 0;
-        for (int x = 0; x < vision; x++) {
-            for (int y = vision - 1; y < vision + 1; y++) {
+        for (int y = vision - 1; y < vision + 1; y++) {
+            for (int x = 0; x < vision; x++) {
                 // +-----------+
                 // |           |
                 // | # # #     |
@@ -148,8 +211,8 @@ public class Bender extends Alien {
                 westScore += scoringMap[x][y] / (vision - x);
             }
         }
-        for (int x = vision - 1; x < vision + 1; x++) {
-            for (int y = vision; y < fields.length; y++) {
+        for (int y = vision; y < fields.length; y++) {
+            for (int x = vision - 1; x < vision + 1; x++) {
                 // +-----------+
                 // |           |
                 // |           |
@@ -160,8 +223,8 @@ public class Bender extends Alien {
                 southScore += scoringMap[x][y] / (y - vision);
             }
         }
-        for (int x = 0; x < vision; x++) {
-            for (int y = vision; y < fields.length; y++) {
+        for (int y = vision; y < fields.length; y++) {
+            for (int x = 0; x < vision; x++) {
                 // +-----------+
                 // |           |
                 // |           |
@@ -173,10 +236,19 @@ public class Bender extends Alien {
             }
         }
         if (southScore > westScore && southScore > southWestScore) {
+            if (DEBUG) {
+                System.out.println(Move.SOUTH);
+            }
             return Move.SOUTH;
         }
         if (westScore > southScore && westScore > southWestScore) {
+            if (DEBUG) {
+                System.out.println(Move.WEST);
+            }
             return Move.WEST;
+        }
+        if (DEBUG) {
+            System.out.println(Move.SOUTHWEST);
         }
         return Move.SOUTHWEST;
     }
